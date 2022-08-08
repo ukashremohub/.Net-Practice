570f9285c788ce4130c05e987241dd73378fb454
Ukashwaran <80847521+ukashremohub@users.noreply.github.com>
2022-08-08 21:36:50 +0530

    Add files via upload

---
+++ Abstraction/Program.cs
@@ -0,0 +1,60 @@
+﻿
+//Child child = new();
+//child.Cola();
+
+//public abstract class Drinks  
+//{
+//    public abstract void Cola();
+
+
+//}
+
+
+//public class Child : Drinks
+//{
+//    public override void Cola() //Reimplementing 
+//    {
+//        Console.WriteLine("Override");
+//    }
+//}
+
+
+
+
+public abstract class AbsParent
+{
+    public void Addition(int x, int y)
+    {
+        Console.WriteLine(x + y);
+    }
+    public void Subtraction(int x , int y)
+    {
+        Console.WriteLine(x - y);
+    }
+    public abstract void Multiplication(int x, int y);
+    public abstract void Division(int x, int y);
+}
+
+public class AbsChild : AbsParent
+{
+    public override void Division(int x, int y)
+    {
+       Console.WriteLine(x / y);
+    }
+
+    public override void Multiplication(int x, int y)
+    {
+        Console.WriteLine(x * y);
+    }
+
+    static void Main()
+    {
+        AbsChild absChild = new();
+        absChild.Addition(10, 20);
+        absChild.Division(10, 20);
+        AbsParent absParent = absChild;
+        absParent.Subtraction(10, 20);
+        absParent.Multiplication(10, 20);
+    }
+}
+
