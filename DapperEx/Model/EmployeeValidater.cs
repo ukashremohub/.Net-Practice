d7a36ff9ab1a87a530da7d99f4391b81b3d2775b
Ukashwaran <80847521+ukashremohub@users.noreply.github.com>
2022-08-11 15:07:44 +0530

    Add files via upload

---
+++ DapperEx/Model/EmployeeValidater.cs
@@ -0,0 +1,15 @@
+﻿namespace DapperEx.Model;
+public class EmployeeValidater : AbstractValidator<EmployeeModel>
+{
+    public EmployeeValidater()
+    {
+        RuleFor(x => x.Id).NotEmpty().NotNull();
+        RuleFor(x => x.Name).NotEmpty().NotNull();
+        RuleFor(x => x.Role).NotEmpty().NotNull();
+        RuleFor(x => x.Email).NotEmpty().NotNull().EmailAddress();
+        RuleFor(x => x.Address).NotNull().MaximumLength(20)
+            .Must(a => a.ToLower().Contains("number") == true)
+            .WithMessage("Address should contain 'Number'");
+
+    }
+}
