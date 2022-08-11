9cf82db9dd0836b000b89dab65401a0f85cfd2e1
Ukashwaran <80847521+ukashremohub@users.noreply.github.com>
2022-08-11 15:09:02 +0530

    Update Program.cs

--- DapperEx/Program.cs
+++ DapperEx/Program.cs
@@ -13,7 +13,47 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
+    app.UseSwaggerUI();using FluentValidation.AspNetCore;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+Validation(builder);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+#region Methods
+void Validation(WebApplicationBuilder builder)
+{
+    builder.Services.AddFluentValidation(x =>
+    {
+        x.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+       // x.RegisterValidatorsFromAssemblyContaining(typeof(EmployeeModel));
+    });
+
+};
+#endregion
 }
 
 app.UseHttpsRedirection();
